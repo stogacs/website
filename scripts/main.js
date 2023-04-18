eb5daace1359f88ccaa1a267ec61cb9f22394155
vivi <38266782+humboldt123@users.noreply.github.com>
2023-04-18 19:49:17 -0400

    create gallery page

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,5 @@ let day = Math.floor((Date.now() - new Date(new Date().getFullYear(), 0, 0)) / 8
 let navbar = document.getElementById("navbar-links");
 
 let activity = events[day > 310 ? 1 : day > 245 ? 0 : day > 100 ? 1 : day > 30 ? 2 : 1];
-navbar.innerHTML += `\n<li><a href="/${activity.toLowerCase()}">${activity}</a></li>`;
+// Uncomment out when Scratchathon, CodeFest, and hackStoga pages are completed
+// navbar.innerHTML += `\n<li><a href="/${activity.toLowerCase()}">${activity}</a></li>`;
