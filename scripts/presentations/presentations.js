fa67fb04c92ef93f0dca87ebb2b9383af9ba7320
Frankanator8 <pika.and.scratch@gmail.com>
2023-10-17 21:53:26 -0400

    sign up design finished, need to finish up the code to actually sign up

--- scripts/presentations/presentations.js
+++ scripts/presentations/presentations.js
@@ -26,7 +26,6 @@ $.get(link + "getNextDays", (dateData, status) => {
             },
             async: false,
             success: function(meetingData) {
-                console.log(meetingData);
                 desc.innerHTML = `
 <u>Time Slot 1:</u>
 <br>
@@ -45,3 +44,70 @@ Presenters: `+ meetingData[1][1]
 
     }
 });
+
+$.get(link + "getAllPastDays", (dateData, status) => {
+    console.log(dateData);
+    var flex = document.getElementById("pas-flex");
+    for (i in dateData) {
+        var item = document.createElement("span");
+        item.className = "pres-item";
+        flex.appendChild(item);
+
+        var banner = document.createElement("section");
+        banner.className = "col-ban blue-mod";
+        item.appendChild(banner);
+        var title = document.createElement("h1");
+        title.innerHTML = dateData[i];
+        banner.appendChild(title);
+
+        var desc = document.createElement("p");
+        desc.className = "pres-desc"
+        desc.id = "desc-"+dateData[i]
+        item.appendChild(desc);
+        $.ajax({
+            type: "GET",
+            url: link + "requestDayInfo",
+            data: {
+                day:dateData[i]
+            },
+            async: false,
+            success: function(meetingData) {
+                desc.innerHTML = `
+<u>Time Slot 1:</u>
+<br>
+Topic: <u><a href=` + meetingData[0][2] + `>` + meetingData[0][0] + `</a></u>` + `
+<br><br>
+Presenters: `+meetingData[0][1]+`
+<br><br>
+<u>Time Slot 2:</u>
+<br>
+Topic: <u><a href=` + meetingData[1][2] + `>` + meetingData[1][0] + `</a></u>` + `
+<br><br>
+Presenters: `+ meetingData[1][1]
+               ;
+            }
+        });
+
+    }
+});
+
+$.get(link + "getAllFutureDays", (nextDays, status) => {
+    var sel = document.getElementById("dateSelect");
+    for (i in nextDays) {
+        var option = document.createElement('option');
+        option.text = option.value = nextDays[i];
+        sel.add(option);
+    }
+});
+
+
+
+function makeSignup() {
+  var backgr = document.getElementById("signupDialog");
+  backgr.style.display="flex"
+};
+
+function destroySignup() {
+  var backgr = document.getElementById("signupDialog");
+  backgr.style.display="none"
+};
